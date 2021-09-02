353af685213b7ce49f2ca9e5fad1a0e26e2bb678
Océane Monguillon <oceanemonguillon@MacBook-de-Oceane.local>
2021-09-02 13:50:05 +0200

    connexion bdd fonctionnelle

--- back/connexionBdd.js
+++ back/connexionBdd.js
@@ -4,9 +4,18 @@ const mysql = require('mysql');
 // Connexion à MYSQL
 const bdd = mysql.createConnection({
   host: '192.168.64.2',
-  user: 'root',
-  password: '',
+  user: 'oceane3',
+  password: 'password',
   database: 'bddGroupomania'
+});
+
+//verification du fonctionnement
+bdd.connect((err) => {   
+  if (err) {    
+      console.log('erreur de connection',err);
+      return;
+  }
+  console.log('Connecté a mysql');
 });
 
 //export bdd
